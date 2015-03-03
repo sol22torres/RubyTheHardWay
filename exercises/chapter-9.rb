ff04c50fdb9d8e8c75f641039b265f2851b37b09
Soledat Torres <soyeya2204@gmail.com>
2015-03-03 11:57:56 -0600

    Pinting in a diferent way

---
+++ exercises/chapter-9.rb
@@ -0,0 +1,13 @@
+days = "Mon Tue Wed Thu Fri Sat Sun"
+#the 'n' and the '\' will print the netx string to the next line 
+months = "Jan\nFeb\nMar\nApr\nMay\bJun\nJul\nAug"
+
+puts " Here are the days: #{days}"
+puts " Here are teh months: #{months}"
+
+puts """
+There are something going on here.
+With the three double-quote.
+We'll be able to type as much as we like. 
+Even 4 lines if we want, or 5, or 6.
+"""
