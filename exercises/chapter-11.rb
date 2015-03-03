a700c144090690164139fefbcaf4f926c63a7302
Soledat Torres <soyeya2204@gmail.com>
2015-03-03 17:57:22 -0600

    Getting data from users

---
+++ exercises/chapter-11.rb
@@ -0,0 +1,13 @@
+#gets.chomp get the info from the user and don't take de enter
+print "How old are you?"
+age = gets.chomp
+print "How tall are you?"
+heigth = gets.chomp
+print "How much do you weigth?"
+weigth = gets.chomp
+print "What's your favorite color?"
+color = gets.chomp
+print "And your favorite hobbie?"
+hobbie = gets.chomp
+
+puts "You're #{age} old, #{heigth} tall and #{weigth} heavy. Your favorite color is #{color} and your favorite hobbie is #{hobbie} good choice!"
