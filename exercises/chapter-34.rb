d21f11bba274f610ca27ba9713549d9fb35e170c
Soledat Torres <soyeya2204@gmail.com>
2015-03-13 20:45:57 -0600

    Animals and arrays

---
+++ exercises/chapter-34.rb
@@ -0,0 +1,20 @@
+animals = ['bear', 'ruby', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+#I had to run this in the terminal 
+#to check my answers
+#1. The animal at 1 
+ruby = animals [1]
+#2. The third (3rd) animal, I only did this wrong 
+peacock = animals [2]
+#3. The first animal 
+bear = animals [0]
+#4. The animal at 3 
+kangaroo = animals [3]
+#5. The fifth (5th) animal 
+whale = animals [4]
+#6. The animal at 2 
+peacock = animals [2]
+#7. The sixth (6th) animal 
+platypus = animals [5]
+#8. The animal at 4 
+whale = animals [4]
